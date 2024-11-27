f6a38aa234d4dc677c87d095b1099eb8e92cd991
Joshua Rose <joshuarose@gmx.com>
2024-11-28 01:06:28 +1100

    refactor: clean up syntax
    Cleaned up syntax so that calls are spread across more functions instead
    of just being in one or two main functions.

--- src-exe/Main.hs
+++ src-exe/Main.hs
@@ -1,7 +1,5 @@
 module Main where
 
---import Text.Colour.Code
-
 el :: String -> String -> String
 el tag content =
     "<" <> tag <> ">\n" <> content <> "\n</" <> tag <> ">"
@@ -18,17 +16,18 @@ title_ content = el "title" ("\ESC[1;90m" <> ("\t" <> content) <> "\ESC[0m")
 body_ :: String -> String
 body_ content = el "body" content
 
-h1_ :: String -> String
-h1_ content = el "h1" content
+h1_ :: String -> (String)
+h1_ = \content -> el "h1" ("\ESC[1;90m" <> ("\t" <> content) <> "\ESC[0m")
 
-p_ :: String -> String
-p_ content = el "p" ("\ESC[1;90m" <> ("\t" <> content) <> "\ESC[0m")
-
-makeHtml :: String -> String -> String
-makeHtml title paragraph = html_ (head_ (title_ title) <> "\n" <> body_ (p_ paragraph))
-
-myhtml :: String
-myhtml = makeHtml "My page title" "My page content"
+p_ :: String -> (String)
+p_ = \content -> el "p" ("\ESC[1;90m" <> ("\t" <> content) <> "\ESC[0m")
 
 main :: IO()
 main = putStrLn myhtml
+
+myhtml :: String
+myhtml =
+    makeHtml "My page title" (h1_ "Header" <> "\n" <> p_ "My page content")
+
+makeHtml :: String -> String -> String
+makeHtml title content = html_ (head_ (title_ title) <> "\n" <> body_ content)
