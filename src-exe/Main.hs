7e5819459194fc56fa892493ad47ed663213a661
Joshua Rose <joshuarose@gmx.com>
2024-11-28 01:27:19 +1100

    refactor: convert to lambda functions
    convert everything into anonymous functions. I'm going to regret this
    later but I don't care.

--- src-exe/Main.hs
+++ src-exe/Main.hs
@@ -1,36 +1,35 @@
 module Main where
 
--- newtype <type-name> = <constructor> <existing-type>
 newtype Html = Html String
 newtype Structure = Structure String
 
 extractStructureString :: Structure -> String
-extractStructureString (Structure str) = str
+extractStructureString = \(Structure str) -> str
 
 extractHtmlString :: Html -> String
-extractHtmlString (Html str) = str
+extractHtmlString = \(Html str) -> str
 
 el :: String -> String -> String
-el tag content =
+el = \tag -> \content ->
     "<" <> tag <> ">\n" <> content <> "\n</" <> tag <> ">"
 
 html_ :: String -> String
-html_ content = el "html" content
+html_ = \content -> el "html" content
 
 head_ :: String -> String
-head_ content = el "head" content
+head_ = \content -> el "head" content
 
 title_ :: String -> String
-title_ content = el "title" ("\ESC[1;90m" <> ("\t" <> content) <> "\ESC[0m")
+title_ = \content ->  el "title" ("\ESC[1;90m" <> ("\t" <> content) <> "\ESC[0m")
 
 body_ :: String -> String
-body_ content = el "body" content
+body_ = \content -> el "body" content
 
 h1_ :: String -> (String)
-h1_ = \content -> el "h1" ("\ESC[1;90m" <> ("\t" <> content) <> "\ESC[0m")
+h1_ = \content -> el "h1" $ "\ESC[1;90m" <> ("\t" <> content) <> "\ESC[0m"
 
 p_ :: String -> (String)
-p_ = \content -> el "p" ("\ESC[1;90m" <> ("\t" <> content) <> "\ESC[0m")
+p_ = \content -> el "p" $ "\ESC[1;90m" <> ("\t" <> content) <> "\ESC[0m"
 
 main :: IO()
 main = putStrLn myhtml
@@ -40,4 +39,4 @@ myhtml =
     makeHtml "My page title" (h1_ "Header" <> "\n" <> p_ "My page content")
 
 makeHtml :: String -> String -> String
-makeHtml title content = html_ (head_ (title_ title) <> "\n" <> body_ content)
+makeHtml = \title -> \content -> html_ $ head_ (title_ title) <> "\n" <> body_ content
